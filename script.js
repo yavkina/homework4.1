2d1943833570e8e972032027792459a361bf31a4
yavkina <yavkina.nastya88@gmail.com>
2021-03-26 14:39:54 +0300

    add 2 files

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const number = +prompt ('Введите число');
 
 
 function declineMyName (number, myName) {
-let result; 
-let count = number % 100;
-if (count >= 5 && count <= 20){
-    result = myName ['2'];
-} else {
+  let result; 
+  let count = number % 100;
+  if (count >= 5 && count <= 20) {
+    result = myName [2];
+  } else {
     count = count % 10;
     if (count === 1) {
-        result = myName ['0'];
-    } else if (count >= 2 && count <= 4){
-        result = myName ['1'];
+        result = myName [0];
+    } else if (count >= 2 && count <= 4) {
+        result = myName [1];
     } else {
-        result = myName ['2'];
+        result = myName [2];
     }
 }
 return result;
